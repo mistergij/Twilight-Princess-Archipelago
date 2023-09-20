cb7e8b411c0a59ed35de4309fea9a8565e7ff303
MisterGij <mistera2u@gmail.com>
2023-09-20 12:57:02 -0400

    Fully Implement FileCreationSettings

--- worlds/twilight_princess/Randomizer/Logic/FileCreationSettings.py
+++ worlds/twilight_princess/Randomizer/Logic/FileCreationSettings.py
@@ -3,11 +3,13 @@ from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parent.parent))
 from Util.SettingsEncoder import BitsProcessor
+from Util.SettingsEncoder import SettingsEncoder
 from FcSettingsEnums import GameRegion, RandomizeBgm
+from Assets.CLR0.clr0Types import RecolorId
 
 
 class FileCreationSettings:
-    def __init__(self):
+    def __init__(self, bits):
         self.gameRegion = None
         self.includeSpoilerLog = True
         self.randomizeBgm = None
@@ -26,6 +28,8 @@ class FileCreationSettings:
         
         self.lanternGlowColor = None
         
+        self.heartColor = None
+        
         self.aBtnColor = None
         
         self.bBtnColor = None
@@ -38,6 +42,8 @@ class FileCreationSettings:
         
         self.linkHairColor = None
         
+        self.processor = BitsProcessor(bits)
+        
     def _FileCreationSettings(self, bits):
         processor = BitsProcessor(bits)
              
@@ -48,4 +54,28 @@ class FileCreationSettings:
         self.randomizeFanfares = processor.NextBool()
         self.disableEnemyBgm = processor.NextBool()
         
-        self.hTunicHatColor = 
+        self.hTunicHatColor = processor.NextClr0Entry(RecolorId.CMPR)
+        self.hTunicBodyColor = processor.NextClr0Entry(RecolorId.CMPR)
+        self.hTunicSkirtColor = processor.NextClr0Entry(RecolorId.CMPR)
+        self.zTunicHatColor = processor.NextClr0Entry(RecolorId.CMPR)
+        self.zTunicHelmetColor = processor.NextClr0Entry(RecolorId.CMPR)
+        self.zTunicBodyColor = processor.NextClr0Entry(RecolorId.CMPR)
+        self.zTunicScalesColor = processor.NextClr0Entry(RecolorId.CMPR)
+        self.zTunicBootsColor = processor.NextClr0Entry(RecolorId.CMPR)
+        self.lanternGlowColor = processor.NextClr0Entry(RecolorId.Zero)
+        self.heartColor = processor.NextClr0Entry(RecolorId.Zero)
+        self.aBtnColor = processor.NextClr0Entry(RecolorId.Zero)
+        self.bBtnColor = processor.NextClr0Entry(RecolorId.Zero)
+        self.xBtnColor = processor.NextClr0Entry(RecolorId.Zero)
+        self.yBtnColor = processor.NextClr0Entry(RecolorId.Zero)
+        self.zBtnColor = processor.NextClr0Entry(RecolorId.Zero)
+        self.midnaHairBaseColor = processor.NextInt(4)
+        self.midnaHairTipsColor = processor.NextInt(4)
+        self.midnaDomeRingColor = processor.NextInt(4)
+        self.linkHairColor = processor.NextClr0Entry(RecolorId.CMPR)
+        
+    def FromString(self, fcSettingsString):
+        bits = SettingsEncoder().DecodeToBitString(fcSettingsString)
+        fcs = FileCreationSettings()
+        fcs._FileCreationSettings(bits)
+        return fcs
